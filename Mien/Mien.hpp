9a810c8585cb2b1794a851131ed499838b6a7c02
Sifen Zhong <zhongsifen@gmail.com>
2018-06-24 22:23:43 +0800

    before add to 58 and 17

--- Mien/Mien.hpp
+++ Mien/Mien.hpp
@@ -4,11 +4,15 @@
 
 class Mien {
 	const int _nfp = 68;	// number of feature points
-	unsigned char* _p;
+	unsigned char* _p;		// storage for computation
 
 public:
 	Mien(int a = 0x20000) { _p = new unsigned char[a]; }
+	~Mien() { delete[] _p; }
 
-	bool landmark_1(cv::Mat& gray, cv::Rect& face, std::vector<cv::Point>& landmark);
+	bool landmark(cv::Mat& gray, cv::Rect& face, std::vector<cv::Point>& landmark);
 	bool landmark_n(cv::Mat& gray, int& n, std::vector<cv::Rect>& face_n, std::vector<std::vector<cv::Point>>& landmark_n);
+
+	void show_face(cv::Mat & img, cv::Rect & face);
+	void show_landmark(cv::Mat& img, std::vector<cv::Point>& landmark);
 };
